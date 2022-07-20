646afffd665005e715c2d5206ad543a586334f4b
Arshia-jm <arshia.j.m1386@gmail.com>
2022-07-20 08:53:11 +0430

    Done zarinpal

---
+++ S5/script.js
@@ -0,0 +1,9 @@
+// var age = 16;
+// console.log(age);
+// console.log("Arshia-jm");
+var name = "Arshia";
+var age = 16;
+var score = [12, 20, 19, 17, 18, 20];
+var num = null;
+console.log(num);
+console.log(typeof number);
